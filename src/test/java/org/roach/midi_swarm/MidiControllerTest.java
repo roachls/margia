882c74735e1f66cf955f81af10139461be20bbc5
Stevie L. Roach <roachls@yahoo.com>
2025-11-26 13:17:58 -0700

    Disable unit test that won't run on GitHub

--- src/test/java/org/roach/midi_swarm/MidiControllerTest.java
+++ src/test/java/org/roach/midi_swarm/MidiControllerTest.java
@@ -2,11 +2,13 @@ package org.roach.midi_swarm;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class MidiControllerTest {
 
 	@Test
+	@Disabled("Only for local use with a DAW")
 	void testPlayNote() throws InterruptedException {
 		var controller = new MidiController(MidiController.LOOP_MIDI, 30);
 		for (var note : Key.generateKey(Key.CHROMATIC_INTERVALS, List.of(Octave.O1)).notes()) {
