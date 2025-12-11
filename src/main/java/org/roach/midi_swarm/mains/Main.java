c3ec2ad2f3847aa9248d48446d01657797774085
Stevie L. Roach <roachls@yahoo.com>
2025-12-11 11:19:22 -0700

    Add jCommander params to MainStateBased

---
+++ src/main/java/org/roach/midi_swarm/mains/Main.java
@@ -0,0 +1,7 @@
+package org.roach.midi_swarm.mains;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
--- src/main/java/org/roach/midi_swarm/mains/MainStateBased.java
+++ src/main/java/org/roach/midi_swarm/mains/MainStateBased.java
@@ -7,39 +7,65 @@ import org.roach.midi_swarm.*;
 import org.roach.midi_swarm.rules.RandomRule;
 import org.roach.midi_swarm.rules.StateBasedRule;
 
+import com.beust.jcommander.*;
+
 /**
  * A state-based ruleset
  */
 public class MainStateBased {
+
+	@Parameters
+	static class StateBasedParams {
+		@Parameter(names = { "-t", "--tempo" }, description = "Tempo of song in beats-per-minute")
+		int tempo = 60;
+		@Parameter(names = { "-channels", "--numChannels" }, description = "Number of MIDI channels")
+		int numChannels = 8;
+		@Parameter(names = { "-seq", "--sequenceLength" }, description = "Starting sequence length")
+		int startingSequenceLength = 20;
+		@Parameter(names = { "-seed", "--randomSeed" }, description = "Random seed")
+		long randomSeed = 101;
+		@Parameter(names = { "-r", "--rows" }, description = "Number of rows in grid")
+		int numRows = 4;
+		@Parameter(names = { "-c", "--cols" }, description = "Number of columns in grid")
+		int numCols = 4;
+		@Parameter(names = {
+				"--decrementSequenceLength" }, description = "Whether to decrement the sequence length over time")
+		boolean decrementSequenceLength = true;
+		@Parameter(names = {
+				"--tickDecrementLength" }, description = "How often to decrement the sequence length (in ticks)")
+		int tickDecrementCount = 100;
+	}
+
 	/**
 	 * Main entry point
 	 * 
 	 * @param args args[0] = number of musicians
 	 */
 	public static void main(String[] args) {
-		if (args.length < 4) {
-			System.err.println("args: tempo numExternalInstruments startingSeqLength randomSeed");
+		var params = new StateBasedParams();
+		var jCommander = new JCommander(params);
+		try {
+			jCommander.parse(args);
+		} catch (ParameterException e) {
+			System.err.println(e.getMessage());
+			jCommander.usage();
 			return;
 		}
-		var numMusicians = 16;
-		var tempo = Integer.parseInt(args[0]);
-		var numExternalInstruments = Integer.parseInt(args[1]);
-		var startingSequenceLength = Integer.parseInt(args[2]);
-		var randomSeed = Long.parseLong(args[3]);
-		var controller = new MidiController("loopMIDI Port", tempo);
+		var numMusicians = params.numRows * params.numCols;
+		var controller = new MidiController("loopMIDI Port", params.tempo);
 //		var controller = new MidiController(DEFAULT_SYNTH, tempo);
 		var musicians = new ArrayList<Musician>();
 		var rules = new ArrayList<StateBasedRule>();
 		for (int i = 0; i < numMusicians; i++) {
-			var rule = new StateBasedRule(startingSequenceLength, 2);
+			var rule = new StateBasedRule(params.startingSequenceLength, 2);
 			rules.add(rule);
-			var musician = new Musician(i, controller, tempo, i % numExternalInstruments, rule);
+			var musician = new Musician(i, controller, params.tempo, i % params.numChannels, rule);
 			rule.setMusician(musician);
 			musicians.add(musician);
 		}
 
 		var baseKey = Key.CMajor;
-		Key.setRandomSeed(randomSeed);
+		Key.setRandomSeed(params.randomSeed);
 		musicians.get(0).setKey(baseKey.of(Octave.O2.getLow(), Octave.O5.getHigh()));
 		musicians.get(1).setKey(baseKey.of(Octave.O2.getLow(), Octave.O4.getHigh()));
 		musicians.get(2).setKey(baseKey.of(Octave.O_NEG2.getLow(), Octave.O1.getHigh()));
@@ -48,15 +74,15 @@ public class MainStateBased {
 		musicians.get(5).setKey(baseKey.of(Octave.O1.getLow(), Octave.O3.getHigh()));
 		musicians.get(6).setKey(baseKey.of(Octave.O3.getLow(), Octave.O4.getLow() + 2));
 		musicians.get(7).setKey(Key.Chromatic.of(Octave.O1.getLow(), Octave.O1.getHigh()));
-		musicians.get(0+8).setKey(baseKey.of(Octave.O2.getLow(), Octave.O5.getHigh()));
-		musicians.get(1+8).setKey(baseKey.of(Octave.O2.getLow(), Octave.O4.getHigh()));
-		musicians.get(2+8).setKey(baseKey.of(Octave.O_NEG2.getLow(), Octave.O1.getHigh()));
-		musicians.get(3+8).setKey(baseKey.of(Octave.O3.getLow(), Octave.O4.getHigh()));
-		musicians.get(4+8).setKey(baseKey.of(Octave.O4.getLow(), Octave.O5.getHigh()));
-		musicians.get(5+8).setKey(baseKey.of(Octave.O1.getLow(), Octave.O3.getHigh()));
-		musicians.get(6+8).setKey(baseKey.of(Octave.O3.getLow(), Octave.O4.getLow() + 2));
-		musicians.get(7+8).setKey(Key.Chromatic.of(Octave.O1.getLow(), Octave.O1.getHigh()));
-		
+		musicians.get(0 + 8).setKey(baseKey.of(Octave.O2.getLow(), Octave.O5.getHigh()));
+		musicians.get(1 + 8).setKey(baseKey.of(Octave.O2.getLow(), Octave.O4.getHigh()));
+		musicians.get(2 + 8).setKey(baseKey.of(Octave.O_NEG2.getLow(), Octave.O1.getHigh()));
+		musicians.get(3 + 8).setKey(baseKey.of(Octave.O3.getLow(), Octave.O4.getHigh()));
+		musicians.get(4 + 8).setKey(baseKey.of(Octave.O4.getLow(), Octave.O5.getHigh()));
+		musicians.get(5 + 8).setKey(baseKey.of(Octave.O1.getLow(), Octave.O3.getHigh()));
+		musicians.get(6 + 8).setKey(baseKey.of(Octave.O3.getLow(), Octave.O4.getLow() + 2));
+		musicians.get(7 + 8).setKey(Key.Chromatic.of(Octave.O1.getLow(), Octave.O1.getHigh()));
+
 		// mute 2nd and 3rd rows
 		musicians.get(4).setMuted(true);
 		musicians.get(5).setMuted(true);
@@ -67,53 +93,39 @@ public class MainStateBased {
 		musicians.get(10).setMuted(true);
 		musicians.get(11).setMuted(true);
 
-		var numCols = 4;
-		var numRows = 4;
-		/*
-		 * @formatter:off
-		 *  0  1  2  3
-		 *  4  5  6  7
-		 *  8  9 10 11
-		 * 12 13 14 15
-		 * @formatter:on
-		 */
-		for (var row = 0; row < numRows; row++) {
-			for (var col = 0; col < numCols; col++) {
-				var index = row * numCols + col;
+		for (var row = 0; row < params.numRows; row++) {
+			for (var col = 0; col < params.numCols; col++) {
+				var index = row * params.numCols + col;
 				if (col > 0) {
 					musicians.get(index).addPeer(musicians.get(index - 1));
 				}
-				if (col < numCols - 1) {
+				if (col < params.numCols - 1) {
 					musicians.get(index).addPeer(musicians.get(index + 1));
 				}
 				if (row > 0) {
-					musicians.get(index).addPeer(musicians.get(index - numCols));
+					musicians.get(index).addPeer(musicians.get(index - params.numCols));
 				}
-				if (row < numRows - 1) {
-					musicians.get(index).addPeer(musicians.get(index + numCols));
+				if (row < params.numCols - 1) {
+					musicians.get(index).addPeer(musicians.get(index + params.numCols));
 				}
 			}
 		}
-		
-		// add a single random musician that is heard only by #0 and can't hear anyone else
-		var rMusician = new Musician(16, controller, tempo, 0, new RandomRule());
+
+		// add a single random musician that is heard only by #0 and can't hear anyone
+		// else
+		var rMusician = new Musician(16, controller, params.tempo, 0, new RandomRule());
 		rMusician.addPeer(musicians.get(0));
 		musicians.get(numMusicians - 1).addPeer(rMusician);
 		musicians.add(rMusician);
 
-		var transport = new Transport(musicians, tempo, controller);
+		var transport = new Transport(musicians, params.tempo, controller);
 		transport.setControlDawTiming(true);
-		transport.addTickAction(100, () -> rules.forEach(m -> m.decrementSequenceLength()));
-		transport.addTickAction(200, () -> rules.forEach(m -> m.decrementSequenceLength()));
-		transport.addTickAction(300, () -> rules.forEach(m -> m.decrementSequenceLength()));
-		transport.addTickAction(400, () -> rules.forEach(m -> m.decrementSequenceLength()));
-		transport.addTickAction(500, () -> rules.forEach(m -> m.decrementSequenceLength()));
-		transport.addTickAction(600, () -> rules.forEach(m -> m.decrementSequenceLength()));
-		transport.addTickAction(700, () -> rules.forEach(m -> m.decrementSequenceLength()));
-		transport.addTickAction(800, () -> rules.forEach(m -> m.decrementSequenceLength()));
-		transport.addTickAction(900, () -> rules.forEach(m -> m.decrementSequenceLength()));
-		transport.addTickAction(1000, () -> rules.forEach(m -> m.decrementSequenceLength()));
-		transport.addTickAction(1100, () -> rules.forEach(m -> m.decrementSequenceLength()));
+		if (params.decrementSequenceLength) {
+			for (int x = 1; x < params.startingSequenceLength; x++) {
+				transport.addTickAction(x * params.tickDecrementCount,
+						() -> rules.forEach(m -> m.decrementSequenceLength()));
+			}
+		}
 		transport.start();
 
 		try (var scanner = new Scanner(System.in)) {
