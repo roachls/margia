32734a24e630b209699d33f240da1dfe8ea486ba
Stevie L. Roach <roachls@yahoo.com>
2026-04-05 12:11:51 -0600

    Add MuteMusician and UnmuteMusician action

---
+++ src/main/java/org/roach/margia/actions/MuteMusician.java
@@ -0,0 +1,17 @@
+package org.roach.margia.actions;
+
+import org.roach.margia.storage.Options;
+
+/**
+ * @param musicianId ID of the musician to mute
+ */
+public record MuteMusician(int musicianId) implements MusicalAction {
+
+    @Override
+    public void perform() {
+        var musician = Options.getInstance().getMusicians().get(musicianId);
+        if (musician != null)
+            musician.setMuted(true);
+    }
+
+}
